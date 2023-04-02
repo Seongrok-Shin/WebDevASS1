74e37da462f8b13c327d685088c28dfdf9fac457
Seongrok-Shin <82435505+Seongrok-Shin@users.noreply.github.com>
2023-04-02 19:13:46 +1200

    Task 1 has done
    (ADD) index.html
    (ADD) about.html
    (ADD) searchstatusform.html
    (ADD) poststatusform.php
    (ADD) style.css
    (Implement) follow instruction as set up title, heading, Name, Email, declare, and absolute links in index.html.

---
+++ poststatusform.php
@@ -0,0 +1,12 @@
+<body>
+
+<?php
+    <form method="post" action="poststatusform.php">
+    Status Code: <input type="text" name="statusCode"><br>
+    Status: <input type="text" name="status"><br>
+    Share: <input type="radio" name="share" value="public">Public
+    <input type="radio" name="share" value="friends">Friends
+    <input type="radio" name="share" value="onlyMe">Only <Menu></Menu>
+    </form>
+?>
+</body>
