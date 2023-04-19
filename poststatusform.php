6207046f940bb8f7ea9deb74c595422843ac18a4
SeongrokShin <ssr7324@gmail.com>
2023-04-20 05:29:21 +1200

    [Update] poststatusform.php (Changed) Wrong action in form (Changed) nmae of permissions
    [Updated] poststatusprocess.php
    (ADD) include header.php and footer.php
    (REMOVED) validateDate function as the reason below
    (Changed) name of date
    (ADD) list_checkbox as get array from POST
    (Changed) checkbox to implode array
    (Removed) POST allowlike, comment and share
    (ADD) select_table to get query for num_rows.
    (Changed) num_rows are counted by select_table.
    (ADD) pattern to use preg_match with status
    (ADD) new connection checker with select_table query
    (ADD) using checkdate instead of validateDate function as a requierment.
    (ADD) check empty for statuscode, status and date as requirements.
    (ADD) file functions such as fopen, fwrite and fclose for writing sql commands into text file which is called sqlscript.txt
    (ADD) return homepage link

--- poststatusform.php
+++ poststatusform.php
@@ -1,51 +1,53 @@
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" >
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Web Development Assignment 1</title>
 </head>
+
 <body>
     <h1>Status Posting System</h1>
-    <form method="post" action="poststatusform.php">
+    <form method="post" action="poststatusprocess.php">
         <div class="input-container">
             <label for="statusCode">Status Code:</label>
-            <input type="text" name="statusCode" id="statuscode" required pattern="S\d{4}">
+            <input type="text" name="statuscode" id="statuscode" required pattern="S\d{4}">
         </div>
-        
+
         <div class="input-container">
-            <label for="status">Status:</label>    
+            <label for="status">Status:</label>
             <input type="text" name="status" id="status" required pattern="^(?!\s*$)[a-zA-Z0-9,.!? ]+$">
         </div>
-        
+
         <div class="input-container">
             <label>Share:</label>
-                <input type="radio" name="share" value="public" id="public" required>
-                <label for="public">Public</label>
-                <input type="radio" name="share" value="friends" id="friends" required>
-                <label for="friends">Friends</label>
-                <input type="radio" name="share" value="onlyme" id="onlyme" required>
-                <label for="onlyme">Only Me</label>
+            <input type="radio" name="share" value="public" id="public">
+            <label for="public">Public</label>
+            <input type="radio" name="share" value="friends" id="friends">
+            <label for="friends">Friends</label>
+            <input type="radio" name="share" value="onlyme" id="onlyme">
+            <label for="onlyme">Only Me</label>
         </div>
-        
+
         <div class="input-container">
-            <label for="date">Date:</label>    
-            <input type="text" name="date" id="date" required pattern="\d{2}/\d{2}/\d{4}" 
-            title="Date must be in the format of dd/mm/yyyy" value="<?php echo date('d/m/Y'); ?>">
+            <label for="date">Date:</label>
+            <input type="text" name="date" id="date" required pattern="\d{2}/\d{2}/\d{4}"
+                title="Date must be in the format of dd/mm/yyyy" value="<?php echo date('d/m/Y'); ?>">
         </div>
-        
-        <div class="input-container">        
+
+        <div class="input-container">
             <label>Permission:</label>
-            <input type="checkbox" name="allowlike" value="yes" id="allowlike">
+            <input type="checkbox" name="checkbox[]" value="Allow like" id="allowlike">
             Allow Like
-            <input type="checkbox" name="allowcomment" value="yes" id="allowcomment">
+            <input type="checkbox" name="checkbox[]" value="Allow comments" id="allowcomment">
             Allow Comments
-            <input type="checkbox" name="allowshare" value="yes" id="allowshare">
+            <input type="checkbox" name="checkbox[]" value="Allow share" id="allowshare">
             Allow Share
         </div>
-        
-        <div class="input-container">        
+
+        <div class="input-container">
             <button type="submit">Post</button>
-        </div> 
+        </div>
         <div class="input-container">
             <a href="http://rpy1983.cmslamp14.aut.ac.nz/assign1/">
                 Return to Home Page
